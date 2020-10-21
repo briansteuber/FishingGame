0cb980d4e4ee7b0669f23dea01f5e98478b2ea7d
brian <brian.steuber18@gmail.com>
2020-10-20 18:39:41 -0700

    test

--- Fishing Game/ViewController.swift
+++ Fishing Game/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     @IBOutlet var secondsLabel: UILabel!
     
     
-    
+    //
     var gameOver = false
     var timer: Timer? = nil
         // try working wiht this
