ea4c5be6c69cb97bff50b71a4f1512ffb33b56d4
brian <brian.steuber18@gmail.com>
2020-10-20 18:36:12 -0700

    working on game logic and layout

--- Fishing Game/ViewController.swift
+++ Fishing Game/ViewController.swift
@@ -10,7 +10,85 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet var buttons: [UIButton]!
+    @IBOutlet var secondsLabel: UILabel!
+    
+    
+    
+    var gameOver = false
+    var timer: Timer? = nil
+        // try working wiht this
+    var seconds: Int = 60 {
+        didSet {
+            secondsLabel.text = "\(seconds)s"
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    func gameOverFunc() {
+        let alertController = UIAlertController(title: "Game Over", message: "You scored: ", preferredStyle: .alert)
+        // Action for when the Okay button is pressed
+        alertController.addAction(UIAlertAction(title: "Play Again", style: .default, handler: { (action) -> Void in
+            self.seconds = 60
+            self.gameOver = false
+            /*
+            let alertController = UIAlertController(title: "Drop a Line", message: "Tap on a cell to drop your line, then wait and see if you caught a fish!", preferredStyle: .alert)
+             */
+        }))
+    }
+    
+    // timer stops but no alert comes up when game is done
+    func startTimer() {
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true, block: { (timer) in
+             if self.gameOver == false {
+                self.seconds -= 1
+                    if (self.seconds == 0) {
+                        print(self.seconds)
+                        self.gameOver = true
+                        //maybe?
+                        //self.newGamePressed(<#T##sender: UIButton##UIButton#>)
+                        // NOT WORKING
+                        
+                    }
+            }
+           
+
+            
+            // fully qualify external reference names
+            // recall closures "capture" external refernces
+            // closures execute LATER
+            // potentially, the ViewController could be deallocated and the closure tries to refer to its seconds property... bad
+            // closures keep their external references in scope/memory
+        })
+    }
+    
+    func stopTimer() {
+           timer?.invalidate() // optional chaining
+           timer = nil
+       }
+    
+    @IBAction func newGamePressed(_ sender: UIButton) {
+        self.stopTimer()
+        
+        let alertController = UIAlertController(title: "Game Over", message: "You scored: ", preferredStyle: .alert)
+        // Action for when the Okay button is pressed
+        alertController.addAction(UIAlertAction(title: "Play Again", style: .default, handler: { (action) -> Void in
+            self.seconds = 60
+            self.startTimer()
+            
+        }))
+        // Alert the consle when the alert shows on the screen to the user
+        present(alertController, animated: true, completion: { () -> Void in
+            print("Alert Just presented")
+        })
+        print("Bet")
+    }
     
     @IBAction func buttonPressed(_ sender: UIButton) {
         print("Button Pressed")
@@ -18,11 +96,6 @@ class ViewController: UIViewController {
         // 2 ways
         // use an outlet connection
         print("tag: \(sender.tag)")
-        /*
-        if let senderIndex = buttons.firstIndex(of: sender) {
-            print("Sender Index\(senderIndex)")
-        }
-        */
         
         
     }
@@ -30,6 +103,35 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    func intro() {
+        let alertController = UIAlertController(title: "Welcome to Carpet Fishing!", message: "Tap a cell to drop your fishing line in carpet sea. After a second, you'll see if you caught a fish. In 60 seconds, try to get the highest score you can. Here are the available fish in Carpet Sea.", preferredStyle: .alert)
+            // Action for when the Okay button is pressed
+            alertController.addAction(UIAlertAction(title: "Okay", style: .default, handler: { (action) -> Void in
+                self.prepUser()
+            }))
+            // Alert the consle when the alert shows on the screen to the user
+            present(alertController, animated: true, completion: { () -> Void in
+                print("Alert Just presented")
+            })
+        }
+    
+    func prepUser() {
+       let alertController = UIAlertController(title: "Drop a Line", message: "Tap on a cell to drop your line, then wait and see if you caught a fish!", preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "Okay", style: .default, handler: { (action) -> Void in
+            self.startTimer()
+        }))
+        // Alert the consle when the alert shows on the screen to the user
+        present(alertController, animated: true, completion: { () -> Void in
+            print("Alert Just presented")
+        })
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.intro()
+        
     }
 }
 
